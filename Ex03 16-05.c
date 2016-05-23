af0d37ae24f8b3afdad63fb44989787b9e3ba0d6
washedaway <was_xrm@hotmail.com>
2016-05-23 09:43:29 -0300

    Add files via upload

--- Ex03 16-05.c
+++ Ex03 16-05.c
@@ -4,15 +4,22 @@
 
 main() {
 	
-	float nota = 1;
+	int i = 0;
+	float nota = 0, media = 0, soma = 0;
 	
-	while (nota >= 0 || nota <= 10){
+	while (nota >= 0 && nota <= 10){
 		printf("Digite a nota: ");
 		scanf("%f",&nota);
 		printf("Nota = %.1f\n",nota);
 		
+		if (nota >= 0 && nota <= 10){
+			soma += nota;
+			i++;
+		}
 	}
 	
+	media = soma / i;
+	printf("A media das notas eh %.1f",media);
 	
 	getch();
 }
